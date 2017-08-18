1e120b2655bd8e3fc79392cf30bbc5ca9cede42c
EriksonBahr <erikson.bahr@gmail.com>
2017-08-17 22:34:21 -0300

    Initial Commit
    Added a artifact to copy blobs  from a container to a local folder

---
+++ Artifacts/windows-copy-cont-from-azure/CopyBlobsFromContainer.ps1
@@ -0,0 +1,22 @@
+ param (
+    [Parameter(Mandatory=$true)][string]$containerName,
+    [Parameter(Mandatory=$true)][string]$destinationPath,
+    [Parameter(Mandatory=$true)][string]$accountName,
+    [Parameter(Mandatory=$true)][string]$accountKey,
+ )
+
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=[$accountName];AccountKey=[$accountKey]'
+
+$storageAccount = New-AzureStorageContext -ConnectionString $connectionString
+
+$blobs = Get-AzureStorageBlob -Container $containerName -Context $storageAccount
+
+foreach ($blob in $blobs)
+{
+    New-Item -ItemType Directory -Force -Path $destinationPath
+
+    Get-AzureStorageBlobContent `
+    -Container $containerName -Blob $blob.Name -Destination $destinationPath `
+    -Context $storageAccount
+
+}
